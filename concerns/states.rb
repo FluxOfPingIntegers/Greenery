b3205935423fc923b3905aa87e653ec3cff7d2ad
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-03-15 21:22:53 -0400

    save method

--- concerns/states.rb
+++ concerns/states.rb
@@ -2,10 +2,15 @@ class State
   attr_accessor :cities
   attr_reader :name, :abb
 
+  @@all = []
   def initialize(name, abb)
     @name = name
     @abb = abb
     @cities = []
+  end
+
+  def save
+    @@all << self
   end
 
 end
