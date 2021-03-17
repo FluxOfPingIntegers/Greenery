a8019c0899106e4229cc264b4b5a2c3d0029de2b
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-03-17 12:03:22 -0400

    created states

--- concerns/ScraperImporter.rb
+++ concerns/ScraperImporter.rb
@@ -9,7 +9,20 @@ class ScraperImporter
   end
 
   def states
-  
+    states = [
+      ["AK", "Alaska"], ["AL", "Alabama"], ["AR", "Arkansas"], ["AS", "American Samoa"], ["AZ", "Arizona"], 
+       ["CA", "California"], ["CO", "Colorado"], ["CT", "Connecticut"], ["D.C.", "Washington"], ["DE", "Delaware"], 
+       ["FL", "Florida"], ["GA", "Georgia"], ["GU", "Guam"], ["HI", "Hawaii"],  ["IA", "Iowa"], ["ID", "Idaho"], 
+       ["IL", "Illinois"], ["IN", "Indiana"], ["KS", "Kansas"], ["KY", "Kentucky"], ["LA", "Louisiana"], 
+       ["MA", "Massachusetts"], ["MD", "Maryland"], ["ME", "Maine"], ["MI", "Michigan"], ["MN", "Minnesota"], 
+       ["MO", "Missouri"], ["MS", "Mississippi"], ["MT", "Montana"], ["NC", "North Carolina"], ["ND", "North Dakota"], 
+       ["NE", "Nebraska"], ["NH", "New Hampshire"], ["NJ", "New Jersey"], ["NM", "New Mexico"], ["NV", "Nevada"], 
+       ["NY", "New York"], ["OH", "Ohio"], ["OK", "Oklahoma"], ["OR", "Oregon"], ["PA", "Pennsylvania"], ["PR", "Puerto Rico"], 
+       ["RI", "Rhode Island"], ["SC", "South Carolina"], ["SD", "South Dakota"], ["TN", "Tennessee"], ["TX", "Texas"], 
+       ["UT", "Utah"], ["VA", "Virginia"], ["VI", "Virgin Islands"], ["VT", "Vermont"], ["WA", "Washington"], ["WI", "Wisconsin"], 
+       ["WV", "West Virginia"], ["WY", "Wyoming"]
+      ]
+      states.each {|x| State.new("#{x[1]}", "#{x[0]}")}
   end
 
   def zones
@@ -20,7 +33,6 @@ class ScraperImporter
     url = URI.parse("https://en.wikipedia.org/wiki/Hardiness_zone")
     response = Net::HTTP.get(url)
     noko_html = Nokogiri::HTML(response)
-    binding.pry
   end
 
 
