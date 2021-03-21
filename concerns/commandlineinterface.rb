2796561547e6cce6dd817d1ba6630dafb30cfb3a
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-03-20 20:37:22 -0400

    better CLI with optional loop

---
+++ concerns/commandlineinterface.rb
@@ -0,0 +1,24 @@
+class Interface
+    def self.inquiry
+        puts "Please select what state your garden will be in using the states two letter abbreviation capitalized."
+        puts "You can also select Washington D.C. as 'DC', or the country of Peurto Rico as 'PR'."
+        state = gets.chomp
+        abb_array = State.all.map {|x| x.abb}
+        while abb_array.none? {|x| x == state}  
+            puts "Invalid selection.  Select the state your garden will reside using its two capital letter abbreviation."
+            state = gets.chomp
+        end
+        state = State.all.find {|x| x.abb == state}
+        prompt = TTY::Prompt.new
+        selection = prompt.select("Using the arrow keys, please select which city is closest to where your garden will reside and press enter.",
+             state.cities.map{|x| x.name})
+        selection = state.cities.find {|x| x.name == selection }
+        puts "The city you have selected is in the USDA hardiness zone of #{selection.zone.name}."
+        puts "Please consult the internet for the month to plant the following plants near #{selection.name}."
+        c = 1
+        while c < 11
+            puts "#{c}.  #{selection.zone.plants[c-1]}."
+            c += 1
+        end
+    end
+end
--- run.rb
+++ run.rb
@@ -1,23 +1,17 @@
 require_relative './config/environment'
 ScraperImporter.new("Start")
-
-puts "Please select what state your garden will be in using the states two letter abbreviation capitalized."
-puts "You can also select Washington D.C. as 'DC', or the country of Peurto Rico as 'PR'."
-state = gets.chomp
-abb_array = State.all.map {|x| x.abb}
-while abb_array.none? {|x| x == state}  
-    puts "Invalid selection.  Select the state your garden will reside using its two capital letter abbreviation."
-    state = gets.chomp
+Interface.inquiry    
+c = 0
+while c == 0
+  puts "Would you like to make another Selection?  Please type 'y' for yes or 'n' for no."
+  selection = gets.chomp
+  case selection
+    when 'y'
+      Interface.inquiry
+    when 'n'
+      puts "Have a nice day!"
+      c += 1
+    else
+      puts "Invalid selection, please try again!"
+  end
 end
-state = State.all.find {|x| x.abb == state}
-prompt = TTY::Prompt.new
-selection = prompt.select("Using the arrow keys, please select which city is closest to where your garden will reside and press enter.",
-     state.cities.map{|x| x.name})
-selection = state.cities.find {|x| x.name == selection }
-puts "The city you have selected is in the USDA hardiness zone of #{selection.zone.name}."
-puts "Please consult the internet for the month to plant the following plants near #{selection.name}."
-c = 1
-while c < 11
-    puts "#{c}.  #{selection.zone.plants[c-1]}."
-    c += 1
-end     
